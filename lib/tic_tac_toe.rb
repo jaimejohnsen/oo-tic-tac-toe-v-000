ca014ab314956c631298d2400eb6c815b2750caf
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 00:23:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    board[index] == "X" || board[index] == "O"
+    @board[index] == "X" || @board[index] == "O"
   end
 
 # def position_taken?(@board, index)
