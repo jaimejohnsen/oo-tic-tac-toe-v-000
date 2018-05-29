59a1c881c7c8474c86f04b18b33a4f7f4d9371a9
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 00:43:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,7 +73,7 @@ def current_player
   end
 end
 
-  def won?(board)
+  def won?
   WIN_COMBINATIONS.each do |win_combination|
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
