e56b3753d72efcade75862f9ccc75d13a407cc7c
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 00:23:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,14 +35,6 @@ class TicTacToe
     @board[index] == "X" || @board[index] == "O"
   end
 
-# def position_taken?(@board, index)
- #   if @board[index] == " " or @board[index] == "" or @board[index] == nil
-  #  false
-   # else @board[index] == "X" or @board[index] == "O"
-    #true
- #end
-#end
-
 def valid_move?(board, index)
   if index.between?(0, 8) && !position_taken?(board, index)
     true
