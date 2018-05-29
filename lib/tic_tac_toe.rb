6e0b783b35ef3fcddd66d1fa7ff93f31669e9505
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 01:24:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,8 +58,8 @@ end
 def turn_count
  counter = 0 
   @board.each do |boardspace|
-  if boardspace == "X" || boardspace == "O"
-          counter += 1 
+    if boardspace == "X" || boardspace == "O"
+        counter += 1 
     end 
   end
   counter
