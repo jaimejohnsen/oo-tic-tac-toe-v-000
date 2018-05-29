be14739a7a3275df8b482cbe5c100331a76e2113
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 00:37:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,17 @@ def valid_move?(index)
   end
 end
 
-
+def turn(input_to_index)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(index)
+     move(index, current_player(@board))
+     display_board
+  else
+    turn
+end
+end
 
 def turn_count
  counter = 0 
