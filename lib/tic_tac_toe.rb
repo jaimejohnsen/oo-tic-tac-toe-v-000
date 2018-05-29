26b8386e5eb88077583b9812830c9daa8119b5f3
Jaime Johnsen <drivetraindiva@gmail.com>
2018-05-29 00:19:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ class TicTacToe
     @board[index] = value
   end
 
- def position_taken?(index)
+ def position_taken?(@board, index)
     if @board[index] == " " or @board[index] == "" or @board[index] == nil
     false
     else @board[index] == "X" or @board[index] == "O"
